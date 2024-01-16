8a2973f54efb1a89c7336f2a953be91b9a9dc4f8
aasthapit <aa.sthapit@live.com>
2024-01-16 16:44:57 -0700

    Update StopWatch.tsx
    Replaced template values for time with the appropriate references from the calculateTimer function
    
    In order to add consistency to the timer having 2 digits per unit of measure, added logic to prepend a 0
    
    Imported useEffect hook from react so that the time is formatted appropriately on time check

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import StopWatchButton from "./StopWatchButton";
 
 export default function StopWatch() {
+  const [timeInSeconds, setStimeInSeconts] = useState<number>(0);
+  const [timerArray, setTimerArray] = useState<Array<number | string>>([]);
+
+  useEffect(() => {
+    let timerArray: Array<number | string> = calculateTimer(timeInSeconds);
+    setTimerArray(timerArray);
+  }, [timeInSeconds]);
+
   return (
     <div className="stopwatch">
       <h1>StopWatch</h1>
       <div className="time">
-        <span className="hours">0</span>
+        <span className="hours">{timerArray[0]}</span>
         <span className="time-sep">h</span>
-        <span className="minutes">0</span>
+        <span className="minutes">{timerArray[1]}</span>
         <span className="time-sep">m</span>
-        <span className="seconds">0</span>
+        <span className="seconds">{timerArray[2]}</span>
         <span className="time-sep">s</span>
       </div>
       <p className="stopWatchButtons"></p>
       <StopWatchButton />
     </div>
   );
+}
+
+//function to calculate the current time and return the results in an resulting array of [hours, minutes, seconds]
+function calculateTimer(timeInSeconds: number): Array<number | string> {
+  let hours: number = Math.floor(timeInSeconds / 3600);
+  let minutes: number = Math.floor((timeInSeconds - hours * 3600) / 60);
+  let seconds: number = timeInSeconds - hours * 3600 - minutes * 60;
+
+  //in order to consistently display 2 digits, doing a check of the time value. If value is less than 10, prepend a 0 to the value as a string
+  let hoursFormatted = hours < 10 ? `0${hours}` : hours;
+  let minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
+  let secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
+
+  return [hoursFormatted, minutesFormatted, secondsFormatted];
 }
