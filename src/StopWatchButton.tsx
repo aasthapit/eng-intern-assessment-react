2146f333f1c426419f305ff8db4657689fde37e4
aasthapit <aa.sthapit@live.com>
2024-01-16 18:01:29 -0700

    Implemented functionality of Buttons
    Implemented functionality for buttons in StopWatchButton
    Enabled Start Stop and Reset buttons

--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function StopWatchButton() {
+type Props = {
+  setTimeInSeconds: Function;
+};
+
+export default function StopWatchButton(props: Props) {
+  const { setTimeInSeconds } = props;
+  const [intervalId, setIntervalId] = useState<number>(0);
+
+  const handleStartButton = () => {
+    let interval: any = setInterval(() => {
+      setTimeInSeconds((previousState: number) => previousState + 1);
+    }, 1000);
+
+    setIntervalId(interval);
+  };
+
+  const handleStopButton = () => {
+    clearInterval(intervalId);
+  };
+
+  const handleResetButton = () => {
+    clearInterval(intervalId);
+    setTimeInSeconds(0);
+  };
+
   return (
     <div className="buttons">
-      <button>Restart</button>
-      <button>Start/Stop</button>
+      <button onClick={handleStopButton}>Restart</button>
+      <button onClick={handleStartButton}>Start/Stop</button>
+      <button onClick={handleStopButton}>Stop</button>
     </div>
   );
 }
