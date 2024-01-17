0d3ae41e94940c53550f08a9d1b2385d997207c5
aasthapit <aa.sthapit@live.com>
2024-01-16 21:43:02 -0700

    Comments
    Added and updated comments to code

--- src/StopWatch.tsx
+++ src/StopWatch.tsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from "react";
 import StopWatchButton from "./StopWatchButton";
 
+//StopWatch function
 export default function StopWatch() {
+  // State to manage the time in seconds
   const [timeInSeconds, setTimeInSeconds] = useState<number>(0);
+  // State to store the formatted timer array [hours, minutes, seconds]
   const [timerArray, setTimerArray] = useState<Array<number | string>>([]);
 
+  // Effect hook to update the timerArray when timeInSeconds changes
   useEffect(() => {
+    // Calculate the timer and update the state
     let timerArray: Array<number | string> = calculateTimer(timeInSeconds);
     setTimerArray(timerArray);
   }, [timeInSeconds]);
@@ -27,13 +32,15 @@ export default function StopWatch() {
   );
 }
 
-//function to calculate the current time and return the results in an resulting array of [hours, minutes, seconds]
+// Function to calculate the current time and return the results in an resulting array of [hours, minutes, seconds]
 function calculateTimer(timeInSeconds: number): Array<number | string> {
+  // Calculate hours, minutes, and seconds
   let hours: number = Math.floor(timeInSeconds / 3600);
   let minutes: number = Math.floor((timeInSeconds - hours * 3600) / 60);
   let seconds: number = timeInSeconds - hours * 3600 - minutes * 60;
 
-  //in order to consistently display 2 digits, doing a check of the time value. If value is less than 10, prepend a 0 to the value as a string
+  // Format hours, minutes, and seconds to always display 2 digits
+  // in order to consistently display 2 digits, doing a check of the time value. If value is less than 10, prepend a 0 to the value as a string
   let hoursFormatted = hours < 10 ? `0${hours}` : hours;
   let minutesFormatted = minutes < 10 ? `0${minutes}` : minutes;
   let secondsFormatted = seconds < 10 ? `0${seconds}` : seconds;
--- src/StopWatchButton.tsx
+++ src/StopWatchButton.tsx
@@ -1,35 +1,71 @@
 import React, { useState } from "react";
 
+// Define the type for props expected by StopWatchButton component
 type Props = {
   setTimeInSeconds: Function;
 };
 
+// StopWatchButton function
 export default function StopWatchButton(props: Props) {
   const { setTimeInSeconds } = props;
+  // State to manage the interval ID for setInterval
   const [intervalId, setIntervalId] = useState<number>(0);
 
+  // Function to handle the Start button click
   const handleStartButton = () => {
+    // Set up the interval to update timeInSeconds every second
     let interval: any = setInterval(() => {
       setTimeInSeconds((previousState: number) => previousState + 1);
     }, 1000);
-
+    // Disable the Start button and store the interval ID
+    setButtonDisabled(true);
     setIntervalId(interval);
   };
 
+  // Function to handle the Stop button click
   const handleStopButton = () => {
+    // Enable the Start button and clear the interval
+    setButtonDisabled(false);
     clearInterval(intervalId);
   };
 
+  // Function to handle the Restart button click
   const handleResetButton = () => {
+    // Clear the interval and reset timeInSeconds to 0
     clearInterval(intervalId);
     setTimeInSeconds(0);
   };
 
+  // Styles for disabled and enabled buttons
+  const styles = {
+    disabledButton: {
+      display: "none",
+      cursor: "not-allowed",
+    },
+
+    enabledButton: {},
+  };
+
+  //keeps track of state to determine what button is displayed. Will have the start button dissapear on start and replaced with stop button
+  const [isButtonDisabled, setButtonDisabled] = useState(false);
+
   return (
     <div className="buttons">
-      <button onClick={handleStopButton}>Restart</button>
-      <button onClick={handleStartButton}>Start</button>
-      <button onClick={handleStopButton}>Stop</button>
+      <button onClick={handleResetButton}>Restart</button>
+      <button
+        onClick={handleStartButton}
+        style={isButtonDisabled ? styles.disabledButton : styles.enabledButton}
+        disabled={isButtonDisabled}
+      >
+        Start
+      </button>
+      <button
+        onClick={handleStopButton}
+        style={!isButtonDisabled ? styles.disabledButton : styles.enabledButton}
+        disabled={!isButtonDisabled}
+      >
+        Stop
+      </button>
     </div>
   );
 }
